6a84aa6cfe1a2f5fcba1a741c86fb38cfad99ca7
ramon <ramonlindo2311@gmail.com>
2024-02-06 15:53:15 -0300

    feat:add language context

---
+++ src/contexts/language/index.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface Languages {}
+
+export const LanguageContext = createContext<Languages | null>(null);
+
+interface LanguageContextProviderProps {
+  children: ReactNode; // Definindo o tipo da propriedade children como ReactNode
+}
+
+export const LanguageContextProvider: React.FC<
+  LanguageContextProviderProps
+> = ({ children }) => {
+  let [language, setlanguage] = useState('pt-BR');
+  return (
+    <LanguageContext.Provider value={{ language }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguageContext = () => {
+  return useContext(LanguageContext)!;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { LanguageContextProvider } from './contexts/language/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <LanguageContextProvider>
+      <App />
+    </LanguageContextProvider>
+  </React.StrictMode>
+);
