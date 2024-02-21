149f3de1b5e1ea565c85b3e272011dd8a38618cf
ramon <ramonlindo2311@gmail.com>
2024-02-21 17:06:06 -0300

    implement about|sobre page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,9 +1,41 @@
+import './index.css';
 import Header from '../../components/HeaderToBack';
 const About = () => {
   return (
     <>
       <Header />
-      <h1>About</h1>
+      <div className="AboutContainer">
+        <div className="AboutTitleContainer">
+          <div className="tracado"></div>
+          <h1 className="About">About Me</h1>
+          <div className="tracado"></div>
+        </div>
+        <div className="ImageAndInformationContainer">
+          <div className="ImageContainer">
+            <div className="imgTracado">
+              <img
+                src="https://jacaimages.vercel.app/imgs/miscellaneous/peackyblinder.png"
+                alt=""
+                className="My-img"
+              />
+            </div>
+          </div>
+          <div className="informationContainer">
+            <h1>Lore ipsum</h1>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur. Urna sed eu felis sagittis
+              etiam id. Mattis fermentum congue odio etiam duis in. Elementum
+              nibh fames amet nunc. Amet tellus tortor lacus volutpat consequat
+              adipiscing urna consectetur vehicula. Duis turpis et semper mauris
+              at. Facilisis erat pharetra quam fusce auctor cras feugiat.
+              Blandit curabitur ut elit viverra luctus montes morbi elementum
+              leo. Adipiscing cras nec diam bibendum ullamcorper sed turpis.
+              Nibh potenti mauris adipiscing fermentum fermentum vehicula diam
+              vitae lectus.
+            </h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
