97a246f590fce27b40646fdfa17f776edd445792
ramon <ramonlindo2311@gmail.com>
2024-02-22 16:04:12 -0300

    feat: implement contact page and language support

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,9 +1,73 @@
+import './index.css';
 import Header from '../../components/HeaderToBack';
+import * as L from './language';
+import { useState } from 'react';
+
 const Contact = () => {
+  let sendEmail = (): void => {
+    const enderecoEmail = 'ramonlopesdev@gmail.com';
+    const link = `mailto:${enderecoEmail}`;
+    window.open(link);
+  };
+  let sendWhats = () => {
+    const numero = '42999952003';
+    const link = `https://wa.me/${numero}`;
+    window.open(link);
+  };
+
+  let en: L.Texts = L.textEN;
+  let pt: L.Texts = L.textPT;
+  let [language, setLanguage] = useState<L.Texts>(en);
   return (
     <>
       <Header />
-      <h1>Contact</h1>
+      <div className="FullContainerContact">
+        <div className="ContainerContact">
+          <div className="Tracado">.</div>
+          <h1 className="Contact">{language.mycontact}</h1>
+          <div className="Tracado">.</div>
+        </div>
+        <div className="InformationContactContainer">
+          <div className="ContactMe">
+            <h1>{language.contactMe}</h1>
+          </div>
+          <div className="ContainerContacts">
+            <div className="LnContainer">
+              <img
+                className="LnImg"
+                src="https://skillicons.dev/icons?i=gmail"
+              />
+              <h2 className="canClick" onClick={sendEmail}>
+                ramonlopesdev@gmail.com
+              </h2>
+            </div>
+
+            <div className="LnContainer">
+              <img
+                className="LnImg"
+                src="https://skillicons.dev/icons?i=linkedin"
+              />
+              <a
+                href="https://www.linkedin.com/in/ramon-lopes-santana/"
+                target="_blank"
+                rel="Ramon Lopes linkedin"
+              >
+                <h2 className="canClick">Ramon Lopes</h2>
+              </a>
+            </div>
+            <div className="LnContainer">
+              <img
+                className="LnImg"
+                src="https://jacaimages.vercel.app/imgs/logos/whatsapp.png"
+                alt=""
+              />
+              <h2 className="canClick" onClick={sendWhats}>
+                (42) 999952003
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/pages/contact/language.ts
@@ -0,0 +1,13 @@
+export type Texts = {
+  contactMe: string;
+  mycontact: string;
+};
+
+export let textEN: Texts = {
+  contactMe: 'Contact me',
+  mycontact: 'My Contacts',
+};
+export let textPT: Texts = {
+  contactMe: 'Contate me',
+  mycontact: 'Meus contato',
+};
