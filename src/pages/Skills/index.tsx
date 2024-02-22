8b8fe1f0ebbf3447f2572021aa529b72dd6ec62b
ramon <ramonlindo2311@gmail.com>
2024-02-22 17:00:34 -0300

    feat: implement language support for skills page

--- src/pages/Skills/index.tsx
+++ src/pages/Skills/index.tsx
@@ -1,17 +1,21 @@
 import './index.css';
 import * as T from '../../mocks/skills';
+import * as L from './language';
 import Header from '../../components/HeaderToBack';
 import { useState } from 'react';
 const Skills = () => {
   let sk: T.Technology[] = T.technology;
+  const pt: L.Texts = L.textPT;
+  const en: L.Texts = L.textEN;
   let [skills, setSkills] = useState<T.Technology[]>(sk);
+  let [language, setLanguage] = useState<L.Texts>(en);
   return (
     <>
       <Header />
       <div className="containerAllSkills">
         <div className="mySkillsTitleContainer">
           <div className="Tracado"></div>
-          <h1 className="MySkillsH1">My Skills</h1>
+          <h1 className="MySkillsH1">{language.skills}</h1>
           <div className="Tracado"></div>
         </div>
         <div className="mySkills">
---
+++ src/pages/Skills/language.ts
@@ -0,0 +1,10 @@
+export type Texts = {
+  skills: string;
+};
+
+export let textEN: Texts = {
+  skills: 'My Skills',
+};
+export let textPT: Texts = {
+  skills: 'Minhas Proficiências',
+};
