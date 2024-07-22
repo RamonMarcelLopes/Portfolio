9f600a8f4f562a83a77cf67681c8daf745c46624
ramon <ramonlindo2311@gmail.com>
2024-07-22 15:22:01 -0300

    add new works

--- src/mocks/works.ts
+++ src/mocks/works.ts
@@ -113,4 +113,70 @@ export const works: Work[] = [
       },
     ],
   },
+  {
+    id: '3b5e9c2d-3f3f-42e2-8k49-051e5un',
+    title: 'CDA EMBLEMAS',
+    img: 'https://jacaimages.vercel.app/imgs/miscellaneous/cda-front.png',
+    github: 'https://github.com/RamonMarcelLopes/CDA-CLIENT',
+    hasDeploy: false,
+    deployLink: '',
+    description: `Sistema que permite aos usuários resgatar emblemas aleatórios e visualizar os emblemas já resgatados em um dashboard`,
+    technologiesFront: [
+      {
+        name: 'REACT',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/React-Dark.svg',
+      },
+
+      {
+        name: 'CSS3',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/CSS.svg',
+      },
+      {
+        name: 'Axios',
+        img: 'https://user-images.githubusercontent.com/8939680/57233884-20344080-6fe5-11e9-8df3-0df1282e1574.png',
+      },
+      {
+        name: 'Vite',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/Vite-Dark.svg',
+      },
+      {
+        name: 'Type script',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/TypeScript.svg',
+      },
+    ],
+    technologiesBack: [],
+  },
+  {
+    id: '3b5e9c2d-3f3f-42e2-8b49-051e8on',
+    title: 'CDA SERVER',
+    img: 'https://jacaimages.vercel.app/imgs/miscellaneous/CDA_SERVER.png',
+    github: 'https://github.com/RamonMarcelLopes/CDA-SERVER',
+    hasDeploy: false,
+    deployLink: '',
+    description: `Sistema feito para o back-end do CDA EMBLEMAS que permite criar, editar e deletar usuários.
+Usuários podem criar, editar, deletar e resgatar emblemas aleatórios`,
+    technologiesFront: [],
+    technologiesBack: [
+      {
+        name: 'Type script',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/TypeScript.svg',
+      },
+      {
+        name: 'Nest JS',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/NestJS-Dark.svg',
+      },
+      {
+        name: 'Prisma',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/Prisma.svg',
+      },
+      {
+        name: 'Mysql',
+        img: 'https://github.com/tandpfun/skill-icons/raw/main/icons/MySQL-Dark.svg',
+      },
+      {
+        name: 'Swagger',
+        img: 'https://static-00.iconduck.com/assets.00/swagger-icon-512x512-halz44im.png',
+      },
+    ],
+  },
 ];
